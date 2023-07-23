267417558a3b21b1f3fbd591256e70fed5152f04
Irina2304 <110673165+Irina2304@users.noreply.github.com>
2023-07-23 22:19:47 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ul = document.querySelector("#ingredients")
+const list = [];
+
+ingredients.forEach(el => {
+  const li = document.createElement("li")
+  li.classList.add("item")
+  li.textContent = el;
+  list.push(li);
+});
+
+ul.append(...list);
+
